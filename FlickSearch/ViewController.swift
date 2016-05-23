c6e35395c8de78b7337c35ab3e06b9648e4f928d
Lloyd Kevin Urbino <lloyd.alvaro@u-hop.com>
2016-05-23 09:30:51 +0800

    Initial Commit

---
+++ FlickSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickSearch
+//
+//  Created by Lloyd Kevin Urbino on 5/23/16.
+//  Copyright © 2016 Lloyd Kevin Urbino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
